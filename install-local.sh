2b62b5fd03490bf0f898d34a6f7697a1c1c9ff18
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2022-04-22 12:31:45 -0400

    Added output log for the datasets loading.

--- install-local.sh
+++ install-local.sh
@@ -1423,7 +1423,8 @@ function install_ckan {
 
       # import the datasets
       printf "${SPACER}${Cyan}${INDENT}Import Datasets${NC}${SPACER}"
-      ckanapi load datasets -I ${APP_ROOT}/backup/od-do-canada.jsonl.gz -z -p 16 -c ${APP_ROOT}/ckan/${CKAN_ROLE}/${CKAN_ROLE}.ini
+      ckanapi load datasets -I ${APP_ROOT}/backup/od-do-canada.jsonl.gz -z -p 16 -c ${APP_ROOT}/ckan/${CKAN_ROLE}/${CKAN_ROLE}.ini -l ${APP_ROOT}/backup/${CKAN_ROLE}__dataset_import__$(date +%s).log
+      chown ckan:ckan ${APP_ROOT}/backup/${CKAN_ROLE}__dataset_import__*.log
 
     fi
     # END
