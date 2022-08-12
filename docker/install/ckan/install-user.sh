ef1e218ac50b58e6acccb7a719ee7e9cb0678bbc
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2022-08-12 13:24:54 -0400

    Updated ckan install users script for 2.9; Removed canada_activity from ckan configs.

--- docker/install/ckan/install-user.sh
+++ docker/install/ckan/install-user.sh
@@ -9,23 +9,23 @@ if [[ $installLocalUser_CKAN == "true" ]]; then
 
         # create member user
         printf "${SPACER}${Cyan}${INDENT}Create default user${NC}${SPACER}"
-        ckan user add user_local email=temp+user@tbs-sct.gc.ca password=12345678 -c $REGISTRY_CONFIG
+        ckan -c $REGISTRY_CONFIG user add user_local email=temp+user@tbs-sct.gc.ca password=12345678
 
         # create system admin
         printf "${SPACER}${Cyan}${INDENT}Create system admin user${NC}${SPACER}"
-        ckan user add admin_local email=temp@tbs-sct.gc.ca password=12345678 -c $REGISTRY_CONFIG
-        ckan sysadmin -c $REGISTRY_CONFIG -v add admin_local password=12345678 email=temp@tbs-sct.gc.ca
+        ckan -c $REGISTRY_CONFIG user add admin_local email=temp@tbs-sct.gc.ca password=12345678
+        ckan -c $REGISTRY_CONFIG sysadmin add admin_local password=12345678 email=temp@tbs-sct.gc.ca
 
     elif [[ $CKAN_ROLE == 'portal' ]]; then
 
         # create member user
         printf "${SPACER}${Cyan}${INDENT}Create default user${NC}${SPACER}"
-        ckan user add user_local email=temp+user@tbs-sct.gc.ca password=12345678 -c $PORTAL_CONFIG
+        ckan -c $PORTAL_CONFIG user add user_local email=temp+user@tbs-sct.gc.ca password=12345678
 
         # create system admin
         printf "${SPACER}${Cyan}${INDENT}Create system admin user${NC}${SPACER}"
-        ckan user add admin_local email=temp@tbs-sct.gc.ca password=12345678 -c $PORTAL_CONFIG
-        ckan sysadmin -c $PORTAL_CONFIG -v add admin_local password=12345678 email=temp@tbs-sct.gc.ca
+        ckan -c $PORTAL_CONFIG user add admin_local email=temp@tbs-sct.gc.ca password=12345678
+        ckan -c $PORTAL_CONFIG sysadmin add admin_local password=12345678 email=temp@tbs-sct.gc.ca
 
     fi
 
